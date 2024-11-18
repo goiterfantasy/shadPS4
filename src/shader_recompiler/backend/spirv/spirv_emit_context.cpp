d739f13922661e0c0b225ecd7b118feeb0a72ef2
Dmugetsu <diegoavilafacebook75@gmail.com>
2024-11-17 19:30:09 -0600

    Fixed Error on booting

--- src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
+++ src/shader_recompiler/backend/spirv/spirv_emit_context.cpp
@@ -384,7 +384,7 @@ void EmitContext::DefineInputs() {
                 continue;
             }
             const Id id{
-                DefineInput(F32[4], 31 - index)}; // TODO: this should usually work, but might
+                DefineInput(F32[4], 16 - index)}; // TODO: this should usually work, but might
             // overlap with per-vertex outputs.
             // Safer solution might require recompiling for every hull/domain permutation to make
             // sure patch variables match in location
@@ -463,7 +463,7 @@ void EmitContext::DefineOutputs() {
             if (!(info.uses_patches & (1U << index))) {
                 continue;
             }
-            const Id id{DefineOutput(F32[4], 31 - index)};
+            const Id id{DefineOutput(F32[4], 16 - index)};
             Decorate(id, spv::Decoration::Patch);
             Name(id, fmt::format("patch_out{}", index));
             patches[index] = id;
